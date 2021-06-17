8862b19bf3da1278bb4ee0eedd23f933d4914f8b
mwiarda <michael+git@wiarda.digital>
2021-06-17 14:06:59 +0200

    added tests

--- test/watchful.test.ts
+++ test/watchful.test.ts
@@ -1,9 +1,9 @@
 import { expect as cdk_expect, haveResource } from '@aws-cdk/assert';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import { Stack } from '@aws-cdk/core';
-import { Watchful } from '../src';
+import { Watchful, WatchfulProps } from '../src';
 
-test('creates an empty dashboard', () => {
+test('creates an empty dashboard by default', () => {
   // GIVEN
   const stack = new Stack();
 
@@ -12,6 +12,32 @@ test('creates an empty dashboard', () => {
 
   // THEN
   cdk_expect(stack).to(haveResource('AWS::CloudWatch::Dashboard'));
+});
+
+test('dashboard prop can be used to disable the creation of a dashboard', () => {
+  // GIVEN
+  const stack = new Stack();
+  const props : WatchfulProps = { dashboard: false };
+
+  // WHEN
+  new Watchful(stack, 'watchful', props);
+
+  // THEN
+  cdk_expect(stack).notTo(haveResource('AWS::CloudWatch::Dashboard'));
+});
+
+test('should fail if dashboard name is specified but dashboard is disabled', () => {
+  // GIVEN
+  const stack = new Stack();
+  const props : WatchfulProps = { dashboard: false, dashboardName: 'Test' };
+
+  // WHEN
+  const wf = () => {
+    new Watchful(stack, 'watchful', props);
+  };
+
+  // THEN
+  expect(wf).toThrow(Error);
 });
 
 test('alarmActions can be used to specify a list of custom alarm actions', () => {
